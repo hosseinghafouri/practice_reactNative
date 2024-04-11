39731aea5f977780c0df9cad790859268780b540
Hossein <81298314+hosseinghafouri@users.noreply.github.com>
2024-04-11 04:57:27 -0700

    finished season 4

--- my-app/App.js
+++ my-app/App.js
@@ -18,116 +18,46 @@ import {
   Alert,
   Modal,
 } from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 function App() {
-  const [modalVisable, setModalVisable] = useState(false);
   return (
-    <View style={styles.container}>
-      <StatusBar hidden={false} backgroundColor={"lightblue"} />
-      <Pressable
-        style={styles.button}
-        onPress={() => {
-          Alert.alert("توجه", "آیا قبول دارید که تریدرید؟", [
-            {
-              text: "بله",
-              onPress: () => {
-                Alert.alert("بکیرم");
-              },
-            },
-            {
-              text: "خیر",
-              onPress: () => {
-                Alert.alert("آفرین گی");
-              },
-            },
-          ]);
+    <View
+      style={{
+        backgroundColor: "#FFF",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        flexWrap: "wrap"
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
         }}
-      >
-        <Text style={styles.textButton}>Open Alert</Text>
-      </Pressable>
-      <Pressable
-        style={styles.button}
-        onPress={() => {
-          setModalVisable(!modalVisable);
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
         }}
-      >
-        <Text style={styles.textButton}>Open M</Text>
-      </Pressable>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisable}
-        onRequestClose={() => {
-          Alert.alert;
-          ("modal has been closed");
-          setModalVisable(!modalVisable);
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
         }}
-      >
-        <View
-          style={{
-            borderWidth: 2,
-            borderBlockColor: "white",
-            backgroundColor: "#fcfc",
-            padding: 5,
-            margin: 5,
-          }}
-        >
-          <Text>Hello</Text>
-          <Image
-            source={{ uri: "https://reactnative.dev/img/showcase/facebook.webp" }}
-            style={{ width: 100, height: 100 }}
-          ></Image>
-                <Pressable
-        style={styles.button}
-        onPress={() => {
-          setModalVisable(!modalVisable);
-        }}
-      >
-        <Text style={styles.textButton}>بستن</Text>
-      </Pressable>
-        </View>
-      </Modal>
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#efefef",
-  },
-  text: {
-    color: "#000",
-    fontSize: 20,
-    margin: 12,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 20,
-  },
-  input: {
-    width: 250,
-    height: 40,
-    margin: 12,
-    padding: 5,
-    fontSize: 20,
-    borderWidth: 3,
-    borderRadius: 10,
-  },
-  textButton: {
-    backgroundColor: "lightgreen",
-    color: "#000",
-    width: 100,
-    height: 30,
-    textAlign: "center",
-    fontSize: 20,
-    borderRadius: 5,
-    margin: 12,
-  },
-  button: { elevation: 4 },
-});
+const styles = StyleSheet.create({});
 
 export default App;
